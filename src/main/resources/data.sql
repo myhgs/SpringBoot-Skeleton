ddd15767257817b111bd21a6097ba43e1ed07454
kiseokhong <vhdpqj12@gmail.com>
2020-02-09 20:34:11 +0900

    Database 초기 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO MEMBER (
+    ID
+    , NAME
+    , AGE
+) VALUES (
+    'test'
+    ,'홍길동'
+    ,'10'
+);
+INSERT INTO MEMBER (
+  ID
+  , NAME
+  , AGE
+) VALUES (
+  'test2'
+  ,'김기석'
+  ,'20'
+);
+INSERT INTO MEMBER (
+  ID
+  , NAME
+  , AGE
+) VALUES (
+  'test3'
+  ,'김길동'
+  ,'30'
+);
