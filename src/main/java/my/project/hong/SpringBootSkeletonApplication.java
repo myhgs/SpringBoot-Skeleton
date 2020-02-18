f01a32c8c3ac8e58f7fec5e326c734f90efd6393
kiseokhong <vhdpqj12@gmail.com>
2020-02-18 21:47:00 +0900

    spring aop 적용

--- src/main/java/my/project/hong/SpringBootSkeletonApplication.java
+++ src/main/java/my/project/hong/SpringBootSkeletonApplication.java
@@ -3,8 +3,10 @@ package my.project.hong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @EnableCaching
 public class SpringBootSkeletonApplication {
 
